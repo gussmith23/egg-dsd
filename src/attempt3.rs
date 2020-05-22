6a343c97d2221b31c5eca575b606078eeab02b85
Gus Smith <guscomps@gmail.com>
2020-05-22 08:58:21 -0700

    Add test

--- src/attempt3.rs
+++ src/attempt3.rs
@@ -112,7 +112,7 @@ pub fn add_strand_to_egraph(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use egg::EGraph;
+    use egg::{EGraph, Pattern, Searcher};
 
     #[test]
     fn add_to_egraph() {
@@ -131,5 +131,50 @@ mod tests {
         );
 
         //egraph.dot().to_svg("tmp.svg").unwrap();
+    }
+
+    #[test]
+    fn search() {
+        let mut egraph = EGraph::<Language, ()>::default();
+        add_strand_to_egraph(
+            &mut egraph,
+            TopOrBottom::Bottom,
+            0,
+            &vec![
+                Domain::Toehold(DomainId::DomainId(0)),
+                Domain::Long(DomainId::DomainId(1)),
+                Domain::Long(DomainId::DomainId(2)),
+                Domain::Long(DomainId::Complement(Box::new(DomainId::DomainId(2)))),
+                Domain::Long(DomainId::DomainId(3)),
+            ],
+        );
+
+        add_strand_to_egraph(
+            &mut egraph,
+            TopOrBottom::Top,
+            0,
+            &vec![
+                Domain::Toehold(DomainId::DomainId(0)),
+                Domain::Long(DomainId::DomainId(1)),
+                Domain::Long(DomainId::DomainId(2)),
+                Domain::Long(DomainId::DomainId(4)),
+            ],
+        );
+
+        //egraph.dot().to_svg("tmp2.svg").unwrap();
+
+        let bottom_pattern: Pattern<Language> = "(bottom-strand-cell (toehold-domain ?a) ?rest)"
+            .parse()
+            .unwrap();
+        let top_pattern: Pattern<Language> = "(top-strand-cell (toehold-domain ?a) ?rest)"
+            .parse()
+            .unwrap();
+
+        let bottom_matches = bottom_pattern.search(&egraph);
+        assert_eq!(bottom_matches.len(), 1);
+        assert_eq!(bottom_matches[0].substs.len(), 1);
+        let top_matches = top_pattern.search(&egraph);
+        assert_eq!(top_matches.len(), 1);
+        assert_eq!(top_matches[0].substs.len(), 1);
     }
 }
