dc0e6736c1bd7158a0f35680e68961723df87ba8
Gus Smith <guscomps@gmail.com>
2020-05-22 08:52:24 -0700

    Complement goes directly on domain id, not on domain.

--- src/attempt3.rs
+++ src/attempt3.rs
@@ -1,10 +1,12 @@
 use egg::{define_language, EGraph, ENode, Id};
 
-type DomainId = u32;
+enum DomainId {
+    Complement(Box<DomainId>),
+    DomainId(u32),
+}
 type StrandId = u32;
 
 enum Domain {
-    Complement(Box<Domain>),
     Toehold(DomainId),
     Long(DomainId),
 }
@@ -25,15 +27,18 @@ define_language! {
         BottomStrandCell = "bottom-strand-cell",
         TopStrandCell = "top-strand-cell",
 
-        // domain: [(complement <domain>)
-        //          | (long-domain <domain-id>)
+        // domain: [ (long-domain <domain-id>)
         //          | (toehold-domain <domain-id>)]
-        Complement = "complement",
         LongDomain = "long-domain",
         ToeholdDomain = "toehold-domain",
 
+        // domain-id: [(complement <domain-id>)
+        //            | <DomainId>]
+        Complement = "complement",
+        // TODO(gus) give an alias for u32 here?
+        DomainId(u32),
+
         Nil = "nil",
-        DomainId(DomainId),
         StrandId(StrandId),
     }
 }
@@ -52,27 +57,30 @@ fn add_strand_to_egraph(
     let first_strand_cell_id: Id = strand_values.iter().rev().fold(
         nil_id,
         |next_strand_cell_enode_id: Id, domain: &Domain| {
+            fn add_domain_id_to_egraph(
+                egraph: &mut EGraph<Language, ()>,
+                domain_id: &DomainId,
+            ) -> Id {
+                match &domain_id {
+                    &DomainId::Complement(domain_id) => {
+                        let domain_id_egraph_id: Id = add_domain_id_to_egraph(egraph, domain_id);
+                        egraph.add(ENode::new(Language::Complement, vec![domain_id_egraph_id]))
+                    }
+                    &DomainId::DomainId(id) => egraph.add(ENode::leaf(Language::DomainId(*id))),
+                }
+            }
             fn add_domain_to_egraph(egraph: &mut EGraph<Language, ()>, domain: &Domain) -> Id {
                 match &domain {
-                    &Domain::Complement(domain) => {
-                        let domain_egraph_id: Id = add_domain_to_egraph(egraph, domain);
-                        egraph.add(ENode::new(Language::Complement, vec![domain_egraph_id]))
-                    }
                     &Domain::Toehold(id) => {
-                        let domain_id_enode_id: Id =
-                            egraph.add(ENode::leaf(Language::DomainId(*id)));
+                        let domain_id_enode_id: Id = add_domain_id_to_egraph(egraph, id);
                         egraph.add(ENode::new(
                             Language::ToeholdDomain,
                             vec![domain_id_enode_id],
                         ))
                     }
                     &Domain::Long(id) => {
-                        let domain_id_enode_id: Id =
-                            egraph.add(ENode::leaf(Language::DomainId(*id)));
-                        egraph.add(ENode::new(
-                            Language::LongDomain,
-                            vec![domain_id_enode_id],
-                        ))
+                        let domain_id_enode_id: Id = add_domain_id_to_egraph(egraph, id);
+                        egraph.add(ENode::new(Language::LongDomain, vec![domain_id_enode_id]))
                     }
                 }
             }
@@ -110,11 +118,11 @@ mod tests {
             TopOrBottom::Bottom,
             0,
             &vec![
-                Domain::Toehold(0),
-                Domain::Long(1),
-                Domain::Long(2),
-                Domain::Complement(Box::new(Domain::Long(2))),
-                Domain::Long(3),
+                Domain::Toehold(DomainId::DomainId(0)),
+                Domain::Long(DomainId::DomainId(1)),
+                Domain::Long(DomainId::DomainId(2)),
+                Domain::Long(DomainId::Complement(Box::new(DomainId::DomainId(2)))),
+                Domain::Long(DomainId::DomainId(3)),
             ],
         );
 
